72c020692ffa6169a58ab85af450ba4d6d74e6b2
Ihor <vep2014@ukr.net>
2023-09-30 20:27:20 +0300

    OutBoxPattern (#1)
    * Add packages for projects
    
    * Setup default pattern for order
    
    * Setup Background service for outbox pattern
    
    ---------
    
    Co-authored-by: Petrukhno, Ihor <ipetrukhno@deloitte.com>

---
+++ OutBoxPattern.Sample/BackgroundServices/EmailBackgroundService.cs
@@ -0,0 +1,53 @@
+﻿using OutBoxPattern.Sample.Services;
+
+namespace OutBoxPattern.Sample.BackgroundServices;
+
+public class EmailBackgroundService : BackgroundService
+{
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+    private readonly ILogger<EmailBackgroundService> _logger;
+    
+    public EmailBackgroundService(
+        IServiceScopeFactory serviceScopeFactory,
+        ILogger<EmailBackgroundService> logger)
+    {
+        _serviceScopeFactory = serviceScopeFactory;
+        _logger = logger;
+    }
+    
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var timer = new System.Timers.Timer();
+        timer.Interval = TimeSpan.FromMinutes(1).TotalMilliseconds;
+        timer.Elapsed += Timer_Elapsed;
+        timer.Start();
+        await Task.Delay(Timeout.Infinite, stoppingToken);
+    }
+    private async void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+    {
+        _logger.LogInformation("Messages are sending.");
+        Send();
+        await Task.Yield();
+    }
+    
+    private void Send()
+    {
+        using (var scope = _serviceScopeFactory.CreateScope())
+        {
+            var emailService = scope.ServiceProvider.GetRequiredService<IMailService>();
+            var emailOutboxService = scope.ServiceProvider.GetRequiredService<IEmailOutbox>();
+            var allOutboxResult = emailOutboxService.GetAll();
+            if (allOutboxResult.Any())
+            {
+                foreach (var item in allOutboxResult)
+                {
+                    var res = emailService.Send(item.Order.Email, "Order is completed", "Your order has been saved in the database", false);
+                    if(res)
+                    {
+                        var updateResult = emailOutboxService.Update(item).Result;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ OutBoxPattern.Sample/Controllers/OrderController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OutBoxPattern.Sample.Models;
+using OutBoxPattern.Sample.Services;
+
+namespace OutBoxPattern.Sample.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrderController: ControllerBase
+{
+    private readonly IOrderService _orderService;
+    private readonly IMailService _mailService;
+    private readonly IEmailOutbox _emailOutbox;
+
+    public OrderController(IOrderService orderService, IMailService mailService, IEmailOutbox emailOutbox)
+    {
+        _orderService = orderService;
+        _mailService = mailService;
+        _emailOutbox = emailOutbox;
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Post(Order order)
+    {
+        var result = await _orderService.AddOrder(order);
+        if (result == null)
+        {
+            return BadRequest();
+        }
+        
+        // Send email if order store in the database
+        var send = _mailService.Send(result.Email, "Order is completed", "Your order has been saved in the database", false);
+        if (!send)
+        {
+            // store in the email outbox
+            EmailOutbox emailOutbox = new EmailOutbox
+            {
+                OrderId = result.Id,
+                Success = false
+            };
+            var outbox = await _emailOutbox.Add(emailOutbox);
+            return Ok(outbox);
+        }
+        return Ok(result);
+    }
+}
