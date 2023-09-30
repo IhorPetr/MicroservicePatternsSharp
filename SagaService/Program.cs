a52031e3b57b60fa8fa4c365e6f61831175d80ed
Petrukhno, Ihor <ipetrukhno@deloitte.com>
2023-09-30 23:51:31 +0300

    Finish default implementation

--- SagaService/Program.cs
+++ SagaService/Program.cs
@@ -1,4 +1,22 @@
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using SagaService;
+using SagaStateMachine;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DbConnection");
+builder.Services.AddDbContextPool<AppDbContext>(db => db.UseSqlServer(connectionString));
+builder.Services.AddMassTransit(cfg =>
+{
+    cfg.AddBus(provider=> MessageBrokers.RabbitMQProvider.ConfigureBus(provider));
+    cfg.AddSagaStateMachine<TicketStateMachine, TicketStateData>()
+        .EntityFrameworkRepository(r =>
+        {
+            r.ConcurrencyMode = ConcurrencyMode.Pessimistic; // or use Optimistic, which requires RowVersion
+
+            r.ExistingDbContext<AppDbContext>();
+        });
+});
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
---
+++ SagaStateMachine/GenerateTicketEvent.cs
@@ -0,0 +1,25 @@
+﻿using Events.Ticket;
+
+namespace SagaStateMachine;
+
+public class GenerateTicketEvent : IGenerateTicketEvent
+{
+    private readonly TicketStateData _ticketStateData;
+    public GenerateTicketEvent(TicketStateData ticketStateData)
+    {
+        _ticketStateData = ticketStateData;
+    }
+    public Guid TicketId => _ticketStateData.TicketId;
+
+    public string Title => _ticketStateData.Title;
+
+    public string Email => _ticketStateData.Email;
+
+    public DateTime RequireDate => _ticketStateData.TicketCreatedDate;
+
+    public int Age => _ticketStateData.Age;
+
+    public string Location => _ticketStateData.Location;
+
+    public string TicketNumber => _ticketStateData.TicketNumber;
+}
